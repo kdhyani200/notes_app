d4b7b14002c615e2627e6f24e15b4565bb642790
Karan Dhyani <karandhyani2000@gmail.com>
2025-02-07 22:25:33 +0530

    Navigation Animation Added

--- app/src/main/java/kd/dhyani/notesapp/Navigation/NotesNavigation.kt
+++ app/src/main/java/kd/dhyani/notesapp/Navigation/NotesNavigation.kt
@@ -1,5 +1,9 @@
 package kd.dhyani.notesapp.Navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,7 +13,32 @@ import kd.dhyani.notesapp.Screens.MainScreen
 
 @Composable
 fun NotesNavigation(navHostController: NavHostController){
-    NavHost(navController = navHostController, startDestination = "home"){
+    NavHost(navController = navHostController, startDestination = "home",
+        enterTransition = {
+            fadeIn(animationSpec = tween(500)) + slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Left,
+                tween((500))
+            )
+        },
+        exitTransition = {
+            fadeOut(animationSpec = tween(500)) + slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.Left,
+                tween((500))
+            )
+        },
+        popEnterTransition = {
+            fadeIn(animationSpec = tween(500)) + slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Right,
+                tween((500))
+            )
+        },
+        popExitTransition = {
+            fadeOut(animationSpec = tween(500)) + slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.Right,
+                tween((500))
+            )
+        }
+        ){
         composable(NotesNavigationItem.HomeScreen.route) {
             MainScreen(navHostController)
         }
